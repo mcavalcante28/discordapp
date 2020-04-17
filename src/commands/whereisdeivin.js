29e1b6f40f8c40977362d7f452d81b1fd36777a2
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-04-17 17:04:02 -0300

    Alterações no comando whereisdeivin

--- src/commands/whereisdeivin.js
+++ src/commands/whereisdeivin.js
@@ -6,19 +6,26 @@ exports.run = async (client, message) => {
   message.guild.roles.cache.filter(role =>{
     if(message.guild.roles.cache.get(role.id).name === targetRole){
         targetRoleID = role.id;
+        console.log('Role ID: ' + targetRoleID)
+    } if(!targetRoleID){
+      targetRoleID = [];
     }
   })
+  console.log(targetRoleID);
     message.guild.channels.cache.filter((channel, index) => {
-      if(channel.type === 'voice'){
-        channel.members.filter((member, index) =>{
-          member._roles.filter(role =>{
+      if(channel.type === 'voice' && channel.members){
+        channel.members.filter(member =>{
+          member._roles.find(role =>{
             if(role === targetRoleID){
               message.reply(`${member.user.username} está no canal ${channel.name}`)
+            } else{ 
+              message.reply(`Deivin is not playing`)
             }
           })
         })
+      } else{
+        message.reply(`Deivin is not playing`)
       }
     })
-
 }
 
