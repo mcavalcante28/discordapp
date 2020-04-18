0eae4aa821293634e23e0cf65ed6a4cf2f79ca43
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-04-18 14:17:15 -0300

    src/commands/whereisdeivin.js

--- src/events/presenceUpdate.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (oldMember, newMember) => {
-  
-  const Discord = require("discord.js");
-
-  // console.log(oldMember);
-  // console.log('-------------------------------------');
-  console.log(newMember.guild.presences.cache);
-}
