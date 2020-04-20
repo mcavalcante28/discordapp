9853545d1537b40a896571113ab09a52edae34a2
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-04-20 09:37:07 -0300

    Análise mensal por ano com entrada do usuário, ainda sem checagem de erros

--- src/commands/analysis.js
+++ src/commands/analysis.js
@@ -4,16 +4,17 @@ let idMembers = [];
 let cont = 0;
 let dateMembers = [];
 let entries4Year = []
-let entries4Month = [];
-let test = []
+let entries4Month = [1];
 
 exports.run = async(client, message) => {
-
-
   const currentYear = getYear(new Date());
   const initialYear = getYear(new Date(message.guild.createdAt))
   let arrayYears = [];
-  let arrayMonths = [1,2,3,4,5,6,7,8,9,10,11,12];
+  let arrayMonths = ['Janeiro','Feveireiro','Março',
+                      'Abril','Maio','Junho',
+                      'Julho','Agosto','Setembro',
+                      'Outubro','Novembro','Dezembro'];
+
   let initYear = initialYear
 
   for(var i = 0;i<=(currentYear-initialYear);i++){
@@ -24,7 +25,7 @@ exports.run = async(client, message) => {
     
     if(!member.user.bot){
       idMembers[cont] = member.user.id;
-      cont = cont+1
+      cont++;
     }
   })
   idMembers.map((id, index) =>{
@@ -32,29 +33,49 @@ exports.run = async(client, message) => {
   })
   const ordernedDates = dateMembers.sort(compareAsc);
 
-  arrayYears.map((year, index) => {
-    entries4Year[index] = ordernedDates.filter(date => getYear(date) === year);
-  })
-  
-  arrayYears.map((year, index_i) => {
-    arrayMonths.map((month, index)=>{
-      entries4Month[index] = ordernedDates.filter(date => getMonth(date) === index && getYear(date) === year);
-    })
-  })
 
-  console.log(entries4Month)
-  const Embed = new Discord.MessageEmbed()
-  .setTimestamp()
-  .setTitle(`${message.guild.name}`)
-  .setColor("RANDOM")
-  .setDescription(`Crescimento do servidor ${message.guild.name}`)
-  .addFields(arrayYears.map((year,index) =>{
-    return(
-      { name: `${year}`, value: `${entries4Year[index].length}`, inline:true}
-    )
-  })
-		
-	)
-message.reply(Embed);
+  const args = message.content.slice(1).split(' ');
+  
+  if(args.length >= 2){
+    switch (args[1]){
+      case 'mensal':
+        arrayMonths.map((month, index)=>{
+          entries4Month[index] = ordernedDates.filter(date => getMonth(date) === index && getYear(date) === parseInt(args[2]));
+        })
+        const Embed1 = new Discord.MessageEmbed()
+        .setTimestamp()
+        .setTitle(`${message.guild.name}`)
+        .setColor("RANDOM")
+        .setDescription(`Contagem de novos membros por mês no ano de ${args[2]}`)
+        .addFields(arrayMonths.map((month ,index) =>{
+          return(
+            { name: `${arrayMonths[index]}`, value: `${entries4Month[index].length}`, inline:true}
+          )
+        })
+        )
+        message.reply(Embed1);
+        break;
+
+
+      case 'anual':
+        arrayYears.map((year, index) => {
+          entries4Year[index] = ordernedDates.filter(date => getYear(date) === year);
+        })
+        const Embed2 = new Discord.MessageEmbed()
+        .setTimestamp()
+        .setTitle(`${message.guild.name}`)
+        .setColor("RANDOM")
+        .setDescription(`Crescimento do servidor ${message.guild.name}`)
+        .addFields(arrayYears.map((year,index) =>{
+          return(
+            { name: `${year}`, value: `${entries4Year[index].length}`, inline:true}
+          )
+        })
+        )
+        message.reply(Embed2);
+        break;
+    }
+  }
+
 
 }
